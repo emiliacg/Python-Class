669b64b54f73ee25ebe9da4d298a543c061613d6
Emilia <emiliacortigiron@gmail.com>
2021-09-07 22:06:54 -0400

    cree una agenda de nombres

--- reforzamiento.py
+++ reforzamiento.py
@@ -1,2 +1,11 @@
 print("yey")
 print("Hello")
+
+
+print("Bienvenidos a la agenda de prueba")
+print ("version alfa")
+agenda= []
+for i in range(3):
+    Nombre= input("agregue nombre: ")
+    agenda.append(Nombre)
+print(agenda)
