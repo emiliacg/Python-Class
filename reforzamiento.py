6508321a55fd581e171a9b8e3a8b7eef3b860d62
Emilia <emiliacortigiron@gmail.com>
2021-09-07 22:16:16 -0400

    cree una agenda de numeros

--- reforzamiento.py
+++ reforzamiento.py
@@ -9,3 +9,9 @@ for i in range(3):
     Nombre= input("agregue nombre: ")
     agenda.append(Nombre)
 print(agenda)
+
+agenda= []
+for i in range(4):
+    Numeros= input("agregue numero: ")
+    agenda.append(Numeros)
+print(agenda)
