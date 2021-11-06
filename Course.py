396448c5f8bc800f17abf25ba16c2528ca59a767
Emilia <emiliacortigiron@gmail.com>
2021-11-05 20:30:24 -0400

    .\Course.py

--- Course.py
+++ Course.py
@@ -19,3 +19,17 @@ u= 10.989
 j= int(u)
 print(u)
 print(j)
+
+if __name__ == '__main__':
+    n = int(input().strip())
+if n%2!=0:
+    print("Weird")
+elif n > 2 and n < 6:
+    print("Not Weird")
+elif n > 6 and n < 21:
+    print("Weird")
+elif n > 20:
+    print("Not Weird")
+else:
+    ("Weird")
+
