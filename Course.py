3d268fe759be6b2934ef3188d6510dedf32b6c11
Emilia <emiliacortigiron@gmail.com>
2021-11-05 20:45:24 -0400

    .\Course.py

--- Course.py
+++ Course.py
@@ -33,3 +33,15 @@ elif n > 20:
 else:
     ("Weird")
 
+def countdown(n):
+    if n <= 0:
+        print('Blastoff!')
+    else:
+        print(n)
+countdown(n-1)
+
+def print_n(s, n):
+    if n <= 0:
+        return
+    print(s)
+    print_n(s, n-1)
